1277de3532128af7f8c08531e90d29f952c7f122
Juan Carlos <jcarlos@fuentesdev.es>
2022-11-06 20:55:31 +0100

    Add project files.

---
+++ OpenBootcamp-cursocsharp-ejercicio3/Program.cs
@@ -0,0 +1,24 @@
+﻿var client = new Client()
+{
+    FullName = "Juan Carlos Fuentes",
+    Phone = "666223311",
+    Address = "Mijas",
+    Email = "jcarlos@fuentesdev.es",
+    isNewClient = true,
+};
+
+Console.WriteLine(client);
+
+public struct Client
+{
+    public string FullName;
+    public string Phone;
+    public string Address;
+    public string Email;
+    public bool isNewClient;
+
+    public override string ToString()
+    {
+        return $"{FullName} with phone: {Phone} and email: {Email}, live in {Address}.  New Client: {isNewClient}";
+    }
+}
